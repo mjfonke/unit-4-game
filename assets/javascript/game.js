86f611a824bfcfef5ed1cd1ead1b5f0754507615
Miran Jang <miranjang9507@gmail.com>
2019-07-25 11:51:24 -0400

    final touch

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,26 +10,46 @@ var wins = 0;
 
 var losses = 0;
 
-var crystalNum1 = Math.floor(Math.random() * 11) + 1;
+var crystalNum1;
 
-var crystalNum2 = Math.floor(Math.random() * 11) + 1;
+var crystalNum2;
 
-var crystalNum3 = Math.floor(Math.random() * 11) + 1;
+var crystalNum3;
 
-var crystalNum4 = Math.floor(Math.random() * 11) + 1;
+var crystalNum4;
 
 
 function gameStart() {
-    computerNumber = Math.floor(Math.random()*101)+19;
+    computerNumber = Math.floor(Math.random() * 101) + 19;
+
+    crystalNum1 = Math.floor(Math.random() * 11) + 1;
+
+    crystalNum2 = Math.floor(Math.random() * 11) + 1;
+
+    crystalNum3 = Math.floor(Math.random() * 11) + 1;
+
+    crystalNum4 = Math.floor(Math.random() * 11) + 1;
+
     $("#match-number").text(computerNumber);
+
+    $("#total-score").text("0")
+
     $("#gem1").attr("data-crystalvalue", crystalNum1);
+
     $("#gem2").attr("data-crystalvalue", crystalNum2);
+
     $("#gem3").attr("data-crystalvalue", crystalNum3);
+
     $("#gem4").attr("data-crystalvalue", crystalNum4);
+
     console.log("gem1: " + crystalNum1);
+
     console.log("gem2: " + crystalNum2);
+
     console.log("gem3: " + crystalNum3);
+
     console.log("gem4: " + crystalNum4);
+
     console.log("Number: " + computerNumber);
 }
 gameStart();
@@ -39,15 +59,22 @@ gameStart();
 
 $("#gem1, #gem2, #gem3, #gem4").on("click", function(){
     var crystalValue = ($(this).attr("data-crystalvalue"));
+
     crystalValue = parseInt(crystalValue);
+
     yourScore += crystalValue;
+
     $("#total-score").text(yourScore)
+
     console.log(crystalValue);
+
+
     if (yourScore === computerNumber) {
         wins++
         $("#win-text").text(wins);
         yourScore=0;
-        
+        gameStart();
+        $("#win-lose").html("<h2>You Win!!!</h2>")
         
     }
 
@@ -55,7 +82,8 @@ $("#gem1, #gem2, #gem3, #gem4").on("click", function(){
         losses++
         $("#lose-text").text(losses);
         yourScore=0;
-       return this;
+        gameStart();
+        $("#win-lose").html("<h2>You lost!!!</h2>")
     }
 })
  
