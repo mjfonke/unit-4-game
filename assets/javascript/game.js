1399b5a44292bc4d5d5c7b5683fb739e33942464
Miran Jang <miranjang9507@gmail.com>
2019-07-25 00:07:26 -0400

    needs to work on reset

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,40 +1,67 @@
 $(document).ready(function(){
 
-// computer pick random numbers from 20 to 60.
-var computerNumber = Math.floor(Math.random()* 50) + 20;
+// computer pic random number between 19-120
+var computerNumber;
 
-var numberOptions = [3, 6, 9, 12];
-
-var totalScore;
+// declare variables
+var yourScore = 0;
 
 var wins = 0;
 
 var losses = 0;
 
-$("#match-number").text(computerNumber);
+var crystalNum1 = Math.floor(Math.random() * 11) + 1;
 
-for  (var i = 0; i <numberOptions.length; i++) {
-    $("#crystal").attr(numberOptions[i]);
+var crystalNum2 = Math.floor(Math.random() * 11) + 1;
 
+var crystalNum3 = Math.floor(Math.random() * 11) + 1;
+
+var crystalNum4 = Math.floor(Math.random() * 11) + 1;
+
+
+function gameStart() {
+    computerNumber = Math.floor(Math.random()*101)+19;
+    $("#match-number").text(computerNumber);
+    $("#gem1").attr("data-crystalvalue", crystalNum1);
+    $("#gem2").attr("data-crystalvalue", crystalNum2);
+    $("#gem3").attr("data-crystalvalue", crystalNum3);
+    $("#gem4").attr("data-crystalvalue", crystalNum4);
+    console.log("gem1: " + crystalNum1);
+    console.log("gem2: " + crystalNum2);
+    console.log("gem3: " + crystalNum3);
+    console.log("gem4: " + crystalNum4);
+    console.log("Number: " + computerNumber);
 }
+gameStart();
 
-$("#crystal").on("click", function(){
-    var crystalValue = parseInt(crystalValue);
 
-    totalScore += crystalValue;
 
-    $("#total-score").text(totalScore);
 
-    if (totalScore === computerNumber) {
-        wins++;
-    }   
-
-    else if (totalScore >= computerNumber) {
-        losses++;
+$("#gem1, #gem2, #gem3, #gem4").on("click", function(){
+    var crystalValue = ($(this).attr("data-crystalvalue"));
+    crystalValue = parseInt(crystalValue);
+    yourScore += crystalValue;
+    $("#total-score").text(yourScore)
+    console.log(crystalValue);
+    if (yourScore === computerNumber) {
+        wins++
+        $("#win-text").text(wins);
+        yourScore=0;
+        
+        
     }
 
-    
+    if (yourScore > computerNumber) {
+        losses++
+        $("#lose-text").text(losses);
+        yourScore=0;
+       return this;
+    }
 })
+ 
 
-$()
+
+
+    
+
 });
